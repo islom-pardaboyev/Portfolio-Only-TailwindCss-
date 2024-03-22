2eb97e4dce94c3bcfada18dc02318d05c91b5c93
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-03-22 15:21:01 +0500

    Updated File

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
+const hLinks = document.querySelectorAll("#hLink");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   hamburger.classList.toggle("bg-white");
 });
+
+hLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    hamburger.classList.toggle("bg-white");
+  });
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      screens: {
-        "sm": "480px"
-      },
       spacing: {
-        'big': "48rem"
-      }
+        big: "48rem",
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
     fontFamily: {
-      nunito: ["Nunito", 'sans-serif']
-    }
+      nunito: ["Nunito", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
