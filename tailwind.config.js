fd083ba2df258a1233992f9f9dda5df973dbbc09
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-03-22 14:36:16 +0500

    Updated File

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        "sm": "480px"
+      },
+      spacing: {
+        'big': "48rem"
+      }
+    },
+    fontFamily: {
+      nunito: ["Nunito", 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
